9d4830953ea683c952ea276a7a694fe7665faff4
Andini Papa Sulima <andinipapasulima@gmail.com>
2026-05-10 16:32:26 +0700

    Update database sql lengkap - Andini

--- db_inventaris.sql
+++ db_inventaris.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 03 Bulan Mei 2026 pada 19.46
--- Versi server: 10.4.32-MariaDB
--- Versi PHP: 8.2.12
+-- Generation Time: May 10, 2026 at 11:29 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,44 +24,133 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `barang`
+-- Table structure for table `barang`
 --
 
 CREATE TABLE `barang` (
   `id` int(11) NOT NULL,
-  `nama` varchar(100) DEFAULT NULL,
-  `stok` int(11) DEFAULT NULL
+  `nama` varchar(100) NOT NULL,
+  `stok` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `barang`
+-- Dumping data for table `barang`
 --
 
 INSERT INTO `barang` (`id`, `nama`, `stok`) VALUES
-(1, 'Laptop', 10),
-(2, 'Mouse', 25),
-(3, 'Keyboard', 15),
-(5, 'Monitor', 8);
+(2, 'Proyektor Epson', 3),
+(3, 'Kursi Kantor', 20),
+(4, 'Whiteboard 120x240', 4),
+(5, 'Kamera DSLR Canon', 2),
+(6, 'Mouse Logitech', 10),
+(9, 'Mouse Logitech', 10),
+(10, 'Mouse Logitech', 10),
+(11, 'Mouse Logitech', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `nama_peminjam` varchar(100) NOT NULL,
+  `tanggal_pinjam` date NOT NULL,
+  `tanggal_kembali` date DEFAULT NULL,
+  `status` enum('dipinjam','dikembalikan') DEFAULT 'dipinjam'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id`, `id_barang`, `nama_peminjam`, `tanggal_pinjam`, `tanggal_kembali`, `status`) VALUES
+(2, 3, 'Siti Rahayu', '2025-05-05', NULL, 'dipinjam'),
+(3, 4, 'Ahmad Fauzi', '2025-05-08', '2025-05-10', 'dikembalikan'),
+(4, 4, 'Andini', '2025-05-10', NULL, 'dipinjam'),
+(5, 4, 'Andini', '2025-05-10', NULL, 'dipinjam'),
+(6, 4, 'Andini', '2025-05-10', NULL, 'dipinjam');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `role` enum('admin','petugas') DEFAULT 'petugas'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `role`) VALUES
+(3, 'petugas2', 'petugas456', 'petugas'),
+(4, 'andini', 'andini123', 'petugas'),
+(5, 'admin', 'admin123', 'admin'),
+(10, 'petugas_baru', 'pass123', 'petugas');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `barang`
+-- Indexes for table `barang`
 --
 ALTER TABLE `barang`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_barang` (`id_barang`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `barang`
+-- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
